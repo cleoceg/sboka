fdaf2943b48c3bde8e4d12bc3c5fdb8ba59951f5
Georges <georges.clerc7@gmail.com>
2017-02-13 14:11:44 +0100

    heroku

--- app.js
+++ app.js
@@ -84,5 +84,8 @@ module.exports = app;
 	console.log('SBOK app listening on port 3000!');
 });*/
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 module.exports = app;
