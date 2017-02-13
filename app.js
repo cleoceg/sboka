12237cf763bf73770d74c51c5a3ae0cff7d47860
Georges <georges.clerc7@gmail.com>
2017-02-13 13:01:38 +0100

    heroku

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ var routeApi = null;
 
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
